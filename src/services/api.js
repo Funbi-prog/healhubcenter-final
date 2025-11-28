6e234f4e8c10805a5159afa6032a7d8bfadc29f0
Funbi Akintade <Ayipexfunby@gmail.com>
2025-11-28 09:36:20 +0100

    update all changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,13 @@
 const API_BASE_URL = "http://192.168.0.183:8000/api/v1";
-
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZWFsaHViIiwiZXhwIjoxNzY0Mjc0NzY3fQ.S4SAYAxXE9SgPzmRcMjZdukC-5yw5N2lQxLIOUbIWAE"
 export async function sendMessage(conversationId, message) {
   const response = await fetch(
     `${API_BASE_URL}/conversations/${conversationId}/messages`,
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}`
       },
       body: JSON.stringify({ content: message }),
     }
@@ -15,9 +16,15 @@ export async function sendMessage(conversationId, message) {
   return response.json();
 }
 
-export async function createConversation() {
+export async function createConversation(title) {
   const response = await fetch(`${API_BASE_URL}/conversations`, {
     method: "POST",
+       headers: {
+        "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}`
+      }, 
+      body: JSON.stringify({title}) 
+
   });
 
   return response.json();
@@ -25,7 +32,10 @@ export async function createConversation() {
 
 export async function getMessages(conversationId) {
   const response = await fetch(
-    `${API_BASE_URL}/conversations/${conversationId}/messages`
+    `${API_BASE_URL}/conversations/${conversationId}/messages`, {method:"GET",   headers: {
+        "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}`
+      },}
   );
   return response.json();
 }
