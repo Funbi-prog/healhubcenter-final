0cdb40e8f0c3b80f56c554f9b84fcd2614e27221
Funbi-prog <ayipexfunby@gmail.com>
2026-01-12 14:42:53 -0800

    Fix API endpoints for Bimpe chat

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,43 @@
-const API_BASE_URL = "http://localhost:8000/api/v1";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZWFsaHViIiwiZXhwIjoxNzY0MzI3NTg3fQ.o8ErK6RqPRA1orlWlnOoS-ZX5q3vNmrdJc2Zb8AAnCY"
+const API_BASE_URL = "https://reportorial-fernando-overstraight.ngrok-free.dev/api/v1";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoZWFsaHViIiwiZXhwIjoxNzY0MzI3NTg3fQ.o8ErK6RqPRA1orlWlnOoS-ZX5q3vNmrdJc2Zb8AAnCY";
+
 export async function sendMessage(conversationId, message) {
   const response = await fetch(
     `${API_BASE_URL}/conversations/${conversationId}/messages`,
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json", 
+        "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
       },
       body: JSON.stringify({ content: message }),
     }
   );
-
   return response.json();
 }
 
 export async function createConversation(title) {
   const response = await fetch(`${API_BASE_URL}/conversations`, {
     method: "POST",
-       headers: {
-        "Content-Type": "application/json", 
-        "Authorization": `Bearer ${token}`
-      }, 
-      body: JSON.stringify({title}) 
-
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({title})
   });
-
   return response.json();
 }
 
 export async function getMessages(conversationId) {
   const response = await fetch(
-    `${API_BASE_URL}/conversations/${conversationId}/messages`, {method:"GET",   headers: {
-        "Content-Type": "application/json", 
+    `${API_BASE_URL}/conversations/${conversationId}/messages`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
-      },}
+      },
+    }
   );
   return response.json();
 }
